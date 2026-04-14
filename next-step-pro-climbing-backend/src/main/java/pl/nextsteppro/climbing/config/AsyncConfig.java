571b5cac21fa003b460b74deb6930d9ce1e62a94
Mateusz Nawratek <nextsteppro.team@gmail.com>
2026-04-14 22:50:05 +0200

    perf: optimize performance prior to virtual threads integration

--- next-step-pro-climbing-backend/src/main/java/pl/nextsteppro/climbing/config/AsyncConfig.java
+++ next-step-pro-climbing-backend/src/main/java/pl/nextsteppro/climbing/config/AsyncConfig.java
@@ -1,26 +1,11 @@
 package pl.nextsteppro.climbing.config;
 
-import java.util.concurrent.Executor;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @EnableAsync
 @EnableScheduling
 public class AsyncConfig {
-
-    @Bean
-    public Executor taskExecutor() {
-        var executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(1);
-        executor.setMaxPoolSize(1);
-        executor.setQueueCapacity(50);
-        executor.setThreadNamePrefix("async-mail-");
-        executor.initialize();
-        return executor;
-    }
 }
